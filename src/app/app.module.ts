4d913607bec1f89eb6dde9bcbdde38d8310261b3
Jardiel Sousa <jardiel.sousa@gmail.com>
2022-04-22 22:00:01 -0300

    criação dos components principais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentComponent } from './components/content/content.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { EducationComponent } from './components/education/education.component';
+import { ReferenceComponent } from './components/reference/reference.component';
+import { LanguageComponent } from './components/language/language.component';
+import { InterestsComponent } from './components/interests/interests.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    FooterComponent,
+    HeaderComponent,
+    SummaryComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    EducationComponent,
+    ReferenceComponent,
+    LanguageComponent,
+    InterestsComponent
   ],
   imports: [
     BrowserModule,
